1c99dd309edaf8c64da1df6152776f361b10d03c
ariel colque <ariel.colque.herrera@gmail.com>
2020-05-13 16:27:09 -0400

    file structured enhanced

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
+
+import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/home/pages/login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     LoginComponent
   ],
   imports: [
---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+
+
+
+@NgModule({
+  declarations: [HomeComponent, LoginComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
