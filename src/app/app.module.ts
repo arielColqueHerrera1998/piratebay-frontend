fdb3124b1e1b837ab3daffca075e45be60d0787c
ariel colque <ariel.colque.herrera@gmail.com>
2020-05-13 20:07:29 -0400

    home finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
-import { HomeComponent } from './modules/home/home.component';
-import { LoginComponent } from './modules/home/pages/login/login.component';
-
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./modules/home/pages/home/home.component";
+import { LoginComponent } from "./modules/home/pages/login/login.component";
+import { MatIconModule } from "@angular/material/icon";
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, LoginComponent],
+  imports: [BrowserModule,MatIconModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HomeComponent } from "./pages/home/home.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, MatIconModule],
 })
-export class HomeModule { }
+export class HomeModule {}
